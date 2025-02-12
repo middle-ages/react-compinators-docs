2abd6e62a13bdcbebc305dd4e4e7da4859300ba3
middle-ages <ran@haag59.com>
2025-02-12 14:17:04 -0800

    update

--- README.md
+++
@@ -1,1 +0,0 @@
-# react-compinators-docs
---
+++ assets/DummyButton.stories-CPh5jUP9.js
@@ -0,0 +1,7 @@
+import{a as e,D as a}from"./chunk-D5ZWXAHU-DUr7XdIt.js";import"./jsx-runtime-D_zvdyIk.js";import"./v4-CtRu48qb.js";const i={component:a},o={args:{text:"Button",color:"green",onClick:e("ButtonClick")}};var t,r,n;o.parameters={...o.parameters,docs:{...(t=o.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    text: 'Button',
+    color: 'green',
+    onClick: action('ButtonClick')
+  }
+}`,...(n=(r=o.parameters)==null?void 0:r.docs)==null?void 0:n.source}}};const p=["Story"];export{o as Story,p as __namedExportsOrder,i as default};
