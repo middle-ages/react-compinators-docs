4f9d170e6f657cbd751812eeaf7a79a5076d3079
middle-ages <ran@haag59.com>
2025-02-14 13:44:09 -0800

    update

--- assets/index.stories-bu42V00Q.js
+++
@@ -1,7 +0,0 @@
-import{a as e,D as r}from"./chunk-D5ZWXAHU-DUr7XdIt.js";import{a as n}from"./map-KcuHEjJC.js";import"./jsx-runtime-D_zvdyIk.js";import"./v4-CtRu48qb.js";const m=n(r)({color:"yellow"},"Yellow"),p={component:m,tags:["!autodocs"]},o={name:"assume",args:{text:"Button",onClick:e("ButtonClick")}};var t,a,s;o.parameters={...o.parameters,docs:{...(t=o.parameters)==null?void 0:t.docs,source:{originalSource:`{
-  name: 'assume',
-  args: {
-    text: 'Button',
-    onClick: action('ButtonClick')
-  }
-}`,...(s=(a=o.parameters)==null?void 0:a.docs)==null?void 0:s.source}}};const d=["Story"];export{o as Story,d as __namedExportsOrder,p as default};
