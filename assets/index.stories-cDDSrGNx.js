ad20b84bc7540936370973e5ba61dd7e4c6c1b40
middle-ages <ran@haag59.com>
2025-02-14 13:56:03 -0800

    update

---
+++ assets/index.stories-cDDSrGNx.js
@@ -0,0 +1,7 @@
+import{a,D as e}from"./chunk-D5ZWXAHU-DUr7XdIt.js";import{a as n}from"./map-C9b7t5y8.js";import"./jsx-runtime-D_zvdyIk.js";import"./v4-CtRu48qb.js";const m=n(e)({color:"yellow"},"Yellow"),i={component:m,tags:["!autodocs"]},o={name:"assumeProps",args:{text:"Button",onClick:a("ButtonClick")}};var t,s,r;o.parameters={...o.parameters,docs:{...(t=o.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  name: 'assumeProps',
+  args: {
+    text: 'Button',
+    onClick: action('ButtonClick')
+  }
+}`,...(r=(s=o.parameters)==null?void 0:s.docs)==null?void 0:r.source}}};const d=["Story"];export{o as Story,d as __namedExportsOrder,i as default};
