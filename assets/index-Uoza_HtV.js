8bba47e3c4aab2795f342e17b688239c5704617e
middle-ages <ran@haag59.com>
2025-02-26 19:57:58 -0800

    update

---
+++ assets/index-Uoza_HtV.js
@@ -0,0 +1,1 @@
+import{a as i,h as c}from"./map-lvHR6i6l.js";const m=t=>a=>Object.assign("default"in a?i(t)(a.default):c(t),l(t,a)),l=(t,a)=>{const s=u(a),n={};for(const r of s){const e=r;n[e]=o=>t({...o,...a[e]})}return{...n,variantNames:s}},u=t=>Object.keys(t).filter(a=>a!=="default");export{m as w};
